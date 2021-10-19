4d2364ed283d04b970faae64aea247bf6d724086
BaitingLuo <luobaiting518918@outlook.com>
2021-10-18 20:05:14 -0500

    Update consumer.py

--- VM2/consumer.py
+++ VM2/consumer.py
@@ -15,11 +15,11 @@ import os   # need this for popen
 import time # for sleep
 from kafka import KafkaConsumer  # consumer of events
 import json
-#import couchdb
+import couchdb
 # We can make this more sophisticated/elegant but for now it is just
 # hardcoded to the setup I ehave on my local VMs
-#couch = couchdb.Server('http://admin:1251314qwe@127.0.0.1:5984/')
-#database = couch['weather']
+couch = couchdb.Server('http://admin:1251314qwe@127.0.0.1:5984/')
+database = couch['weather']
 # acquire the consumer
 # (you will need to change this to your bootstrap server's IP addr)
 print("consumer start")
@@ -27,7 +27,7 @@ consumer = KafkaConsumer (bootstrap_servers="129.114.27.120:9092",value_deserial
 
 # subscribe to topic
 consumer.subscribe (topics=["utilizations"])
-file = open("consumer_output.txt", 'w')
+#file = open("consumer_output.txt", 'w')
 # we keep reading and printing
 for msg in consumer:
     # what we get is a record. From this record, we are interested in printing
@@ -41,18 +41,18 @@ for msg in consumer:
     # Note that I am not showing code to obtain the incoming data as JSON
     # nor am I showing any code to connect to a backend database sink to
     # dump the incoming data. You will have to do that for the assignment.
-    #database.save(msg.value)
-    file = open("consumer_output.txt", 'a')
-    str = ""
-    for i in msg.value.items():
-        str += i[0]
-        str += ","
-        str += i[1]
-        str += ","
-    str += "\n"
-    file.write(str)
-    file.close()           
-    print (msg.value)
+    database.save(msg.value)
+    #file = open("consumer_output.txt", 'a')
+    #str = ""
+    #for i in msg.value.items():
+    #    str += i[0]
+    #    str += ","
+    #    str += i[1]
+    #    str += ","
+    #str += "\n"
+    #file.write(str)
+    #file.close()           
+    #print (msg.value)
     time.sleep(1)
 
 # we are done. As such, we are not going to get here as the above loop
